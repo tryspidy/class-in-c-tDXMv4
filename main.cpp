efdd42ee8d76de8e84a549b63ddb424f96381676
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-12 01:04:28 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+class Name {
+  private:
+  	int data;
+  
+  public:
+  
+    // Constructor
+  	Name() {
+		// Code      
+    }
+  	int id;
+  	void fun1(int a) {
+        // Some instructions here 
+    }
+ 	 
+}
